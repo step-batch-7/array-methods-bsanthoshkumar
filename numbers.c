940b2319b8eb6003a6ed684863e1a163064b9244
santhosh <santhosh.kumar@thoughtworks.com>
2020-05-09 15:28:07 +0530

    extracted perform_array_methods function from main funtion in numbers.c

--- numbers.c
+++ numbers.c
@@ -1,13 +1,9 @@
 #include "array.h"
 #include "array_void.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-int main()
+void perform_array_methods(Array_ptr list)
 {
-  int arr[] = {1, 2, 3, 4, 5};
-  Array_ptr list = create_array(arr, 5);
-
   Array_ptr sqr_of_numbers = map(list, &sqr);
   display(sqr_of_numbers);
 
@@ -19,6 +15,13 @@ int main()
 
   int sum_of_numbers = reduce(list, 0, &sum);
   printf("%d \n", sum_of_numbers);
+}
+
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5};
+  Array_ptr list = create_array(arr, 5);
+  perform_array_methods(list);
 
   return 0;
 }
